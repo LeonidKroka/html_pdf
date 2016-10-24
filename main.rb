71a03b9efaadd1b80c0756c75e0f29dc0d0f77f6
Leonid Kroka <kroka_l@ukr.net>
2016-10-24 20:08:46 +0300

    Create pdf file

--- main.rb
+++ main.rb
@@ -1,37 +1,38 @@
-require "erb"
 require 'date'
+require (File.dirname(__FILE__)+'/view_blank/create_pdf')
+include PDF
 
 class LawPrint
-  attr_accessor :stylesheet, :data, :date
-
   def initialize file=File.dirname(__FILE__)+"/default.txt"
     read_data_from file
-    @date = Date.today
+    @data[:date] = Date.today
     @stylesheet = rout "/assets/main.css"
-    create_html
-    html
+    create_pdf link, @data
+    view_pdf
   end
 
   def read_data_from file
     data = File.readlines file
-    @data = { :town => data[0],
-              :seller => data[1],
-              :buyer => data[2],
-              :price => data[3],
-              :specifications => data[4]}
+    @data = { :town => data[0].delete!("\n"),
+              :seller => data[1].delete!("\n"),
+              :buyer => data[2].delete!("\n"),
+              :price => data[3].delete!("\n"),
+              :specifications => data[4].delete!("\n")}
   end
 
-  def create_html
-    view = File.open(rout("/public/law.html"), "w+")
-    view.puts ERB.new(File.read(rout "/view_blank/print.html.erb")).result binding
-  end
-
-  def html
-    spawn("google-chrome #{rout "/public/law.html"}")
+  def view_pdf
+    spawn("xdg-open #{rout "/public/law.pdf"}")
   end
 
   private
     def rout short_url
       File.dirname(__FILE__)+short_url
     end
+
+    def link
+      { :bold => (rout "/assets/font/TimesNewRomanBold.ttf"),
+        :normal  => (rout "/assets/font/TimesNewRomanRegular.ttf") }
+    end
 end
+
+LawPrint.new()
